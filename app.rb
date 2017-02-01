1fbed4d2c27309e65a9fbc7a57ef131c16374f42
j2FunOnly <j2funonly@ya.ru>
2017-02-02 00:57:23 +0300

    Clears tmp dir

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'mini_magick'
+require 'fileutils'
 
 A4_WIDTH = 1654
 A4_HEIGHT = 2339
@@ -7,6 +8,7 @@ THUMB_WIDTH = A4_WIDTH / COLS
 LAST_THUMB_WIDTH = A4_WIDTH - THUMB_WIDTH * (COLS - 1)
 
 images = Dir.glob 'img/*.jpg'
+total_images = 0
 MiniMagick::Tool::Convert.new do |b|
   b.size "#{A4_WIDTH}x#{A4_HEIGHT}"
   b << 'xc:white'
@@ -19,13 +21,17 @@ MiniMagick::Tool::Convert.new do |b|
     thumb_width = (thumb_index == COLS - 1) ? LAST_THUMB_WIDTH : THUMB_WIDTH
     thumb.auto_orient
     thumb.resize "#{thumb_width}x"
-    thumb.write "tmp/tmp_photo_#{i}.jpg"
-    b << "tmp/tmp_photo_#{i}.jpg"
+    thumb_filename = "tmp/tmp_image_#{i}.jpg"
+    thumb.write thumb_filename
+    b << thumb_filename
 
     b.geometry "+#{THUMB_WIDTH * thumb_index}+#{v_offset[thumb_index]}"
     v_offset[thumb_index] += thumb.height
     b.composite
+    total_images = i + 1
     break if v_offset.all? { |k, v| v > A4_HEIGHT }
   end
   b << 'output/events.jpg'
 end
+FileUtils.rm Dir.glob('tmp/*.jpg')
+puts "Complete! Processed #{total_images} image files."
